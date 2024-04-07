32abcff5e8cb982f704e6c160d394de6a0ec0518
Ignacio Gomez <ignacio.gomez2901@gmail.com>
2024-04-07 20:25:05 -0300

    more practice, now using times

---
+++ contracts/Countdown.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract Countdown{
+
+    uint256 private startTime;
+    uint256 private gameDuration = 60; //seconds
+
+    constructor(){
+        startTime = block.timestamp; //actual block
+    }
+
+    function setGameDuration(uint256 time) external{
+        gameDuration = time;        
+    }
+
+    function getDuration() external view returns(uint256){
+        return gameDuration;
+    }
+
+    function _restore () external {
+            startTime = block.timestamp;
+    }
+
+    function timeLeft()public view returns(uint256){
+        require(startTime<= block.timestamp, "Time not valid");
+
+    }
+}
