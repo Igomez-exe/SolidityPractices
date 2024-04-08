86d0ad8c5b69d9e54a059e3b3c4e526885692eaa
Ignacio Gomez <ignacio.gomez2901@gmail.com>
2024-04-08 18:38:37 -0300

    countdown to a simple vote

--- contracts/Countdown.sol
+++ contracts/Countdown.sol
@@ -6,6 +6,13 @@ contract Countdown{
     uint256 private startTime;
     uint256 private gameDuration = 60; //seconds
 
+    struct Voter {
+        uint256 weight;
+        bool voted;
+    }
+
+    mapping(address=>Voter) private voters;
+
     constructor(){
         startTime = block.timestamp; //actual block
     }
@@ -28,5 +35,18 @@ contract Countdown{
         uint256 remaining = diff >= gameDuration ? 0 : gameDuration - diff;
         
         return remaining;
+    }
+
+    //new functions 
+
+    function getVote () external view returns(Voter memory) {
+        return voters[msg.sender];
+    }
+
+    function vote (uint256 _weight) external  {
+        require(timeLeft() > 0, "Time is over");
+        Voter storage voter = voters[msg.sender];
+        voter.voted = true;
+        voter.weight = _weight;
     }
 }
