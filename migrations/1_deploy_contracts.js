5447b4c9ddd7dcddeeee97450ee493e76230eef3
Ignacio Gomez <ignacio.gomez2901@gmail.com>
2024-04-08 01:24:09 -0300

    config

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -1,5 +1,7 @@
 const Manager = artifacts.require("Manager");
+const Countdown = artifacts.require("Countdown"); 
 
 module.exports = function(deployer) {
   deployer.deploy(Manager);
+  deployer.deploy(Countdown);
 };
